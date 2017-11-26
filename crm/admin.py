2731a89af8ab88843291199d94e0c78eefef3b4f
Casa <casa@iMac-de-Casa.local>
2017-11-26 20:34:18 +0100

    Added company serializer

--- crm/admin.py
+++ crm/admin.py
@@ -1,7 +1,17 @@
+"""
+Setting exposed CRM models
+"""
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-
 from django.contrib import admin
-from .models import Company
+from .models import Company, Modules, Roles
 
-admin.site.register(Company)
+@admin.register(Company)
+class CompanyAdmin(admin.ModelAdmin):
+    list_display = ('cif', 'nombre', 'tipo_via', 'direccion1', 'cod_pos', 'provincia')
+
+@admin.register(Modules)
+class ModulesAdmin(admin.ModelAdmin):
+    list_display = ('mod_apl', 'mod_codigo', 'mod_desc', 'mod_version')
+
+admin.site.register(Roles)
--- crm/views.py
+++ crm/views.py
@@ -1,6 +1,6 @@
+"""
+CRM views
+"""
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-
 from django.shortcuts import render
-
-# Create your views here.
