86a0b2d6e9464ad73e1255d005deaf3cd61deb99
Casa <casa@iMac-de-Casa.local>
2017-12-13 22:08:45 +0100

    Reorganizacion modelos

--- prisma/settings/base.py
+++ prisma/settings/base.py
@@ -21,8 +21,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'rest_framework.authtoken',
-    'corsheaders',
     'crm',
+    'corsheaders'
 ]
 
 MIDDLEWARE = [
